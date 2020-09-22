cc5a3396fdee59ded58d25184c8ce2e87d3ba121
unperknown <unperknown@icloud.com>
2020-09-22 10:30:02 +0900

    feat: completed purchased list page

--- src/PurchaseList.js
+++ src/PurchaseList.js
@@ -1,13 +1,39 @@
 import React from 'react';
 
-import './PurchaseList.css'
+import { gql, useQuery } from '@apollo/client';
+import CurrencyFormat from 'react-currency-format';
+
+import './PurchaseList.css';
+
+const FETCH_TRANSACTIONS = gql`
+  query {
+    transactionsInUser {
+      tid
+      amount
+      createdAt
+    }
+  }
+`;
 
 function PurchaseList() {
+  const { loading, error, data } = useQuery(FETCH_TRANSACTIONS);
+
+  if (loading) return <h1>Loading...</h1>;
+
+  if (error) return <p>{error.message}</p>;
+
   return (
     <div className="Purchase">
       <div>
         <ul>
-          <li className='purchase'><div className='time'>2020-01-01 00:00:00</div><div className='amount'>-890809</div></li>
+          {data.transactionsInUser.map((value, index) => {
+            return (
+              <li key={index} className='purchase'>
+                <div className='time'>{Date(value.createdAt)}</div>
+                <CurrencyFormat value={value.amount} displayType={'text'} thousandSeparator={true} prefix={'₩'} renderText={value => <div className='amount'>{value}</div>} />
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
