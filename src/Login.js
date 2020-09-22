bf77b87660d01d22f1840e446235ac5e479855cf
unperknown <unperknown@icloud.com>
2020-09-22 09:52:06 +0900

    feat: completed login page

--- src/Login.js
+++ src/Login.js
@@ -1,27 +1,61 @@
-import React from "react";
+import React from 'react';
 
-import './Login.css'
-import {useHistory} from "react-router";
+import { gql, useMutation } from '@apollo/client';
+import { useHistory } from 'react-router-dom';
 
-let history;
+import './Login.css';
 
-const Login = () => {
-  const id = document.getElementsByName('id');
-  const pw = document.getElementsByName('pw');
-  localStorage.setItem('session', 'a');
-  localStorage.getItem('session')
-  history.push('/purchase');
-}
+const LOGIN = gql`
+  mutation Login($id: String!, $pw: String!) {
+    Login(id: $id, pw: $pw) {
+      data
+    }
+  }
+`;
 
-export default () => {
-  history = useHistory();
+function Login() {
+  let history = useHistory();
+
   if (localStorage.getItem('session')) history.replace('/purchase');
+
+  let id, pw;
+  const [login] = useMutation(LOGIN);
+
   return (
     <div className='Login'>
-      GSM Pay
-      <input type="text" name="id" />
-      <input type="password" name="pw" />
-      <button onClick={Login}>로그인</button>
+      <h1>GSM Pay</h1>
+      <form onSubmit={e => {
+        e.preventDefault();
+
+        login({
+          variables: {
+            id: id.value,
+            pw: pw.value,
+          }
+        }).then(result => {
+          let session = result.data.Login.data;
+
+          if (session == null) return;
+
+          localStorage.setItem('session', session);
+
+          history.push('/purchase');
+        }).catch(err => {
+          console.log(err);
+        });
+      }}>
+        <input type="text" ref={node => {
+          id = node;
+        }} />
+        <br />
+        <input type="password" ref={node => {
+          pw = node;
+        }} />
+        <br />
+        <button type="submit">로그인</button>
+      </form>
     </div>
   )
 }
+
+export default Login;
