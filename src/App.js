d4d1fa7e70fd932f813c6d9a43d3a036bcf009b8
unperknown <unperknown@icloud.com>
2020-09-22 11:09:22 +0900

    style: quotation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
-import {Switch, Route} from 'react-router'
+import { BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router';
 import './App.css';
-import Login from "./Login";
-import Home from "./Home";
+import Login from './Login';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path='/' exact component={Login} />
-          <Route path='' component={Home} />
+          <Route path="/" exact component={Login} />
+          <Route path="" component={Home} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,28 @@
-import {BrowserRouter, useHistory} from "react-router-dom";
-import {Route, Switch} from "react-router";
-import Purchase from "./Purchase";
-import PurchaseList from "./PurchaseList";
-import React from "react";
+import { BrowserRouter, useHistory } from 'react-router-dom';
+import { Route, Switch } from 'react-router';
+import Purchase from './Purchase';
+import PurchaseList from './PurchaseList';
+import React from 'react';
 
-export default () => {
+function Home() {
   let history = useHistory();
-  console.log(history)
+
   if (!localStorage.getItem('session')) history.replace('/');
+
   return (
     <>
       <BrowserRouter>
         <Switch>
-          <Route path='/purchase' exact component={Purchase}/>
-          <Route path='/list' exact component={PurchaseList}/>
+          <Route path="/purchase" exact component={Purchase} />
+          <Route path="/list" exact component={PurchaseList} />
         </Switch>
       </BrowserRouter>
-      <div className='Menu'>
+      <div className="Menu">
         <a href="/purchase">결제</a>
         <a href="/list">내역</a>
       </div>
     </>
-  )
+  );
 }
+
+export default Home;
