b3b3191f66a0f322898267900bce5373a81ff9d7
unperknown <unperknown@icloud.com>
2020-09-22 11:09:57 +0900

    refactor: loading message

--- src/PurchaseList.js
+++ src/PurchaseList.js
@@ -18,7 +18,7 @@ const FETCH_TRANSACTIONS = gql`
 function PurchaseList() {
   const { loading, error, data } = useQuery(FETCH_TRANSACTIONS);
 
-  if (loading) return <h1>Loading...</h1>;
+  if (loading) return <h3>결제 내역을 불러오고 있습니다.<br/>잠시만 기다려주세요.</h3>;
 
   if (error) return <p>{error.message}</p>;
 
