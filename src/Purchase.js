cc55242e87d41295769866be3ffe9a355207240c
unperknown <unperknown@icloud.com>
2020-09-22 11:09:44 +0900

    feat: completed payment page

--- src/Purchase.js
+++ src/Purchase.js
@@ -1,20 +1,53 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
 import './App.css';
 
-const executePurchase = () => {
-  const id = document.getElementsByName('id');
-  const pw = document.getElementsByName('pw');
-  // call api
-}
+import { gql, useMutation } from '@apollo/client';
+
+const CREATE_TRANSACTION = gql`
+  mutation CreateTransaction($bid: Int!, $amount: Int!) {
+    createTransaction(bid: $bid, amount: $amount) {
+      tid
+      amount
+    }
+  }
+`;
 
 function Purchase() {
+  let history = useHistory();
+
+  let bid, amount;
+
+  const [createTransaction] = useMutation(CREATE_TRANSACTION);
+
   return (
     <div className="Purchase">
-      <div>
-        <input type="text" name='bid' />
-        <input type="text" name='amount' />
-        <button onClick={executePurchase}>결제</button>
-      </div>
+      <form onSubmit={e => {
+        e.preventDefault();
+
+        createTransaction({
+          variables: {
+            bid: parseInt(bid.value),
+            amount: parseInt(amount.value),
+          }
+        }).then(_ => {
+          history.replace('/list');
+        }).catch(err => {
+          console.log(err);
+        });
+
+      }}>
+        <input type="text" ref={node => {
+          bid = node;
+        }} />
+        <br/>
+        <input type="text" ref={node => {
+          amount = node;
+        }}/>
+        <br/>
+        <button type="submit">결제</button>
+      </form>
     </div>
   );
 }
